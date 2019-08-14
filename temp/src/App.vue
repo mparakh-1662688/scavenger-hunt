80cde93968c625819a8a73c1b8faed0dce71888e
Mohneel Parakh <mparakh@uw.edu>
2019-08-14 11:20:18 -0700

    test

---
+++ temp/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                video: {},
+                canvas: {},
+                captures: []
+            }
+        },
+        mounted() { },
+        methods: { }
+    }
+</script>
+
+<style>
+    body: {
+        background-color: #F0F0F0;
+    }
+    #app {
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    #video {
+        background-color: #000000;
+    }
+    #canvas {
+        display: none;
+    }
+    li {
+        display: inline;
+        padding: 5px;
+    }
+</style>
